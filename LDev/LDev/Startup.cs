378bb3f7194695a55bc061bc781db285bb1dac77
Luis Esquivel <luisesquivel7f5@gmail.com>
2020-01-15 11:50:05 -0600

    LDEV CON AUTENTICACIÓN DE SOCIAL MEDIA

---
+++ LDev/LDev/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LDev.Startup))]
+namespace LDev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
